62ffa26d37cb08a94b586cb41d2df550f187bfef
Kristina DeSilva <97710440+kavdesilva@users.noreply.github.com>
2025-08-31 09:37:41 +0300

    first commit

---
+++ movies_db/queries.sql
@@ -0,0 +1,23 @@
+\connect movies_lab_db;
+-- Test query!!
+SELECT * FROM users;
+
+-- 1. List all the movie titles and their corresponding directors.
+
+
+
+-- 2. Select the movie title and user name for all of the "favorites" represented by the `users_movies` table.
+
+
+
+-- 3. List the movies with the number of favorites they have.
+
+
+
+-- 4. List the names of directors along with the number of favorites that exist for all of the movies they've made, ordered by number of favorites descending.
+
+
+
+-- 5. List the user name, director name and favorite count of all of the user/director combinations (based on the `users_movies` table).
+
+
